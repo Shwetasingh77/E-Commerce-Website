12d999c592671bb753cb9e045ef2607f58871b01
Shwetasingh77 <shwetasinghrajputbirpur@gmail.com>
2024-05-13 02:40:21 +0530

    FIRST FILE
    FIRST FILE

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Product from './components/Product'
@@ -7,23 +7,28 @@ import SearchItems from './components/SearchItems'
 import Cart from './components/Cart'
 import ProductDetails from './components/ProductDetails'
 import { items } from './components/Data'
+import GooglePayButton from '@google-pay/button-react'
 
-const App =()=>{
+const App = () => {
   const [data, setData] = useState([...items])
+  const [cart, setCart] = useState([])
+
+  // Calculate total amount
+  const totalAmount = cart.reduce((total, item) => total + item.price, 0)
 
   return (
     <>
-    <Router>
-    <Navbar/>
-       <Routes>
-      <Route path="/" element={<Product items={data}/>}></Route>
-      <Route path="/product/:id"element={<ProductDetails/>} />
-      <Route path="/search/:term" element={<SearchItems/>} />
-      <Route path="/cart" element={<Cart/>} />
-       </Routes>
-    </Router>
-    </>
+      <Router>
+        <Navbar cart={cart} setData={setData} />
+        <Routes>
+          <Route path="/" element={<Product cart={cart} setCart={setCart} items={data} />} />
+          <Route path="/product/:id" element={<ProductDetails cart={cart} setCart={setCart} />} />
+          <Route path="/search/:term" element={<SearchItems cart={cart} setCart={setCart} />} />
+          <Route path="/cart" element={<Cart cart={cart} setCart={setCart} />} />
+        </Routes>
+      </Router>
     
+    </>
   )
 }
 
--- src/components/SearchItems.jsx
+++ src/components/SearchItems.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React ,{useEffect,useState}from 'react'
+import { useParams } from 'react-router-dom';
+import { items } from './Data';
+import Product from './Product';
 
-function SearchItems() {
+const SearchItems = (cart,setCart) => {
+   // console.log(useParams())
+  const {term}=useParams();
+  const [filterData, setFilterData] = useState([]);
+
+  useEffect(()=>{
+    const filteredData = () =>{
+      const data= items.filter((p)=>p.title.toLowerCase().includes(term.toLowerCase()));
+      console.log(data);
+      setFilterData(data)
+    }
+    filteredData();
+
+  }, [term])
+  
   return (
-    <div>
-      SearchItems
-    </div>
+    <Product cart={cart} setCart= {setCart} items={filterData}/>
   )
 }
 
